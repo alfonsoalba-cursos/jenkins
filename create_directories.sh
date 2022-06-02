2eb92a15bc12a5fc47e24264446df7f55ecc4086
Alfonso Alba <alfonso@alfonsoalba.com>
2022-06-02 08:17:06 +0200

    Add helper script to copy files from old repositories.

---
+++ create_directories.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+slides_dir="slides/es/$1-$2"
+images_dir="slides/images/es/$1"
+
+[[ ! -d $slides_dir ]] && echo "Creating $slides_dir" &&  mkdir -p $slides_dir
+[[ ! -d $images_dir ]] && echo "Creating $images_dir" &&  mkdir -p $images_dir
+
+echo "Copiando imágenes"
+cp $3/slides/images/* $images_dir
+echo "Copiando diapositivas"
+cp $3/slides/*.md $slides_dir
